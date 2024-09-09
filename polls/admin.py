4ae88ff712d993179f47b5cfc35c11dd49ecce09
GabrielPlus <gabrielpius0311@gmail.com>
2024-09-09 12:24:40 +0300

    starting group work

---
+++ polls/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from .models import Question, Choice
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
+    def was_published_recently(self, obj):
+        return obj.was_published_recently()
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
